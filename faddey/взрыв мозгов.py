a03a5974cbb7dd722493c7a32a666f3b709ce60a
neshcola <gits_ne_school@mail.ru>
2025-12-03 18:20:57 +0300

    Add files via upload

---
+++ faddey/взрыв мозгов.py
@@ -0,0 +1,13 @@
+print('''рукгцщс9i569з96гииетшь8687гщгпгнеoutuebhnvvyнзг875в78шовщмш8г565гмн8888тм858г8знм85989ьгjervtuuykirepyioutpykoivyuijiovuyiyov4l456ubm956uv658v9um-4mumuv4u-m--uup08587096ndbeiypwmbu8vym9530v35yu34vyu8y67988uj8v9n8ny45buohj7oveiygvtu8v6uyu8vwnjovwu85onuyn78v8735on4y5o3v47y2v8o5nu39tu3bm[u90ub9p03uon69y98v4i7u8n5yv5nu9nyvu898nv6bunube5yuv4нсе6ст3етс8н743есщггч9834еч78н89сон79тсетн9сн87е68ек7у583е989ртст84е22е5е3мн7рр8676еjo9o4ju98vcgte''')
+
+x=input()
+if x=='да':
+  print('вы видете компьютер вы его включаете,но он запаролин введите пароль')
+  while True:
+    y=input()
+    if y=='''рукгцщс9i569з96гииетшь8687гщгпгнеoutuebhnvvyнзг875в78шовщмш8г565гмн8888тм858г8знм85989ьгjervtuuykirepyioutpykoivyuijiovuyiyov4l456ubm956uv658v9um-4mumuv4u-m--uup08587096ndbeiypwmbu8vym9530v35yu34vyu8y67988uj8v9n8ny45buohj7oveiygvtu8v6uyu8vwnjovwu85onuyn78v8735on4y5o3v47y2v8o5nu39tu3bm[u90ub9p03uon69y98v4i7u8n5yv5nu9nyvu898nv6bunube5yuv4нсе6ст3етс8н743есщггч9834еч78н89сон79тсетн9сн87е68ек7у583е989ртст84е22е5е3мн7рр8676еjo9o4ju98vcgteFaddey0123456789-+*/=''':
+      break
+    print('не верно')
+  print('это правельно как много игр напремер симулятор Гарри Поттер интересно')
+  z=input()
+  print('вы упали в обморок')
