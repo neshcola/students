413f74240e76d35e2d996d23dee13f85a32cef5f
neshcola <gits_ne_school@mail.ru>
2025-12-17 18:04:23 +0300

    Add files via upload

---
+++ faddey/print kil.py
@@ -0,0 +1,25 @@
+def avg(m):
+    le = 0
+    for i in m:
+        le += 1
+    sum = 0
+    for i in m:
+        sum += i
+    e=sum/le
+    return e
+mas = []
+
+while True:
+    x = input()
+    if x == 'Харе!':
+        break
+    try:
+        x = int(x)
+        mas.append(x)
+    except:
+        print('не понял...')
+result = avg(mas)
+print(result)
+input()    
+  
+    
