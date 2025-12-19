3accc677311bee7766b3dff0d450142add6a9ba4
neshcola <gits_ne_school@mail.ru>
2025-12-19 17:42:47 +0300

    Add files via upload

---
+++ telega.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = "8516795655:AAHfSA9wS3pf4GcOAFxD052HCBMZKrtdIBE"
+
+dp = Dispatcher()
+
+
+# Command handler
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+# Run the bot
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
+          
