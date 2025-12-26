0d15554cc0043dd8424c4ae2f4149cb64c6f5b22
neshcola <gits_ne_school@mail.ru>
2025-12-26 17:03:48 +0300

    Update Teleg.py

--- LemonadeBuissness/Teleg.py
+++ LemonadeBuissness/Teleg.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher                     
 from aiogram.filters import Command
 from aiogram.types import Message
 import asyncio
@@ -71,6 +71,7 @@ async def main():
     await dp.start_polling(bot)
 
 asyncio.run(main())          
+
 
 
 
