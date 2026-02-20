e2d8500a4374d4b53555ae289ade4ad1ca8b717c
neshcola <gits_ne_school@mail.ru>
2026-02-20 16:55:17 +0300

    Update Teleg.py

--- LemonadeBuissness/Teleg.py
+++ LemonadeBuissness/Teleg.py
@@ -7,6 +7,9 @@ TOKEN = "8516795655:AAHfSA9wS3pf4GcOAFxD052HCBMZKrtdIBE"
 
 dp = Dispatcher()
 
+
+
+
 lemons = 0
 money = 50
 lemonade = 0
@@ -17,9 +20,7 @@ price_sugar = 5
 production_lemonade = 5
 
 # Command handler
-@dp.message(Command("start"))
-async def command_start_handler(mes):
-    await mes.answer("Здорово")
+
     
 @dp.message(Command("buy_lemon"))
 async def buy_lemon(mes):
@@ -30,7 +31,7 @@ async def buy_lemon(mes):
         money -= 15
         await mes.answer('Вы получили лимон. Всего лимонов:' + str(lemons))
     else:
-        await mes.answer('недостаточно денег.Всего :' + str(money) + 'рублей')
+        await mes.answer('недостаточно денег. Всего :' + str(money) + 'рублей')
         
 
 @dp.message(Command("buy_sugar"))
@@ -42,7 +43,7 @@ async def buy_sugar(mes):
         money -= 5
         await mes.answer('Вы получили 100 грамм сахара. Всего грамм сахара:' + str(sugar))
     else:
-        await mes.answer('недостаточно денег.Всего :' + str(money) + 'рублей')
+        await mes.answer('недостаточно денег. Всего :' + str(money) + 'рублей')
 
 @dp.message(Command("make_lemonade"))
 async def make_lemonade(mes):
@@ -55,31 +56,44 @@ async def make_lemonade(mes):
         sugar -= 100
         await mes.answer('Вы сделали 1 бутылку лимонада. Всего бутылок лимонада:' + str(lemonade))
     else:
-        await mes.answer('недостаточно лимонов и сахара.Всего :' + str(lemons) + 'лимонов и' + str(sugar) + 'грамм сахара')
+        await mes.answer('недостаточно лимонов и сахара. Всего :' + str(lemons) + 'лимонов и' + str(sugar) + 'грамм сахара')
 @dp.message(Command("inventory"))
 async def inventory(mes):
-    global lemonade
-    global sugar
-    global lemons
-    await mes.answer(str(lemons) + "лимонов" +
-                     str(sugar) + 'грамм сахара' +
-                     str(lemonade) + 'бутылок лимонада' )
-      
+    global lemonade, sugar, lemons
+    await mes.answer(f'{lemons} лимонов.\n{sugar} грамм сахара.\n{lemonade} бутылок лимонада.\n{money} денег')
+
+
+
+
+
+users = {}
+
+@dp.message(Command("start"))
+async def command_start_handler(mes):
+    global users
+    x = mes.chat.id
+    if x in users.keys():
+        await mes.answer("Привет! покупай ингредиенты, делай лимонад и зарабатывай!")
+    else:
+        users[x] = {'Деньги':50,'Лимоны':5} 
+        await mes.answer('Пользователь создан')
+
+
+@dp.message(Command("add_lemon"))
+async def add_lemoons(mes):
+    global users
+    x = mes.chat.id
+    if users[x]['Деньги'] >= 10:
+        users[x]['Деньги'] -= 10
+        users[x]['Лимоны'] += 1
+        await mes.answer(f'Вы купили Лимон! Всего Лимонов: {users[x]['Лимоны']}. Осталось денег: {users[x]['Деньги']}')
+    else:
+        await mes.answer(f'Недостаточно денег. Всего денег: {users[x]['Деньги']}')
+
+
 # Run the bot
 async def main():
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
 asyncio.run(main())          
-
-
-
-
-
-
-
-
-
-
-
-
